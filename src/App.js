104653ed8e4597d666cd8a4765c4e85cbc5b20a3
KuVOle <traviscolt95@me.com>
2023-05-03 23:51:20 +0300

    Add /src/components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 const tgApp = window.Telegram.WebApp;
 
 function App() {
-  const onClose = () => { tgApp.close(); }
+
   useEffect(() => { tgApp.ready() }, []); // приложение проиницилизировано и его можно использловать
   return (
     <div>
       <h1>Shop</h1>
-      <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
