e7531e9c87673f2de1e7756d20c535f4fc209eac
KuVOle <traviscolt95@me.com>
2023-05-04 10:48:23 +0300

    add hooks useTelegramm

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Button from '../Button/Button';
+import { useTelegramm } from '../../hooks/useTelegramm';
+import './Header.css';
 
 const Header = () => {
-    const tgApp = window.Telegram.WebApp;
-    const onClose = () => { tgApp.close(); }
+
+    const { user, onClose } = useTelegramm();
+
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
-            <p>{tgApp}</p>
-            <span className={'username'}>{tgApp?.initDataUnsafe?.user?.username}</span>
+            <span className={'username'}>{user}</span>
         </div>
     );
 }
