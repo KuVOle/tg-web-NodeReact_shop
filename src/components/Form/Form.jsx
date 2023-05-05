1150547154dfea0f7e93a28248d097129e5b1292
KuVOle <traviscolt95@me.com>
2023-05-05 23:08:08 +0300

    add effect mainButton from empty form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
-import './Form.css'
+import React, { useEffect, useState } from 'react';
+import './Form.css';
+import { useTelegramm } from '../../hooks/useTelegramm';
+
 
 const Form = () => {
-    const [coutry, setCountry] = useState('');
+    const [country, setCountry] = useState('');
     const [street, setStreet] = useState('');
     const [subject, setSubject] = useState('physiacal');
+
+    const { tgApp } = useTelegramm();
+
+    // eslint-disable-next-line
+    useEffect(() => { tgApp.MainButton.setParams({ text: 'Отправить данные', }) }, []);
+
+    useEffect(() => {
+        if (!street || !country) {
+            tgApp.MainButton.hide();
+        } else {
+            tgApp.MainButton.show();
+        }
+    }, []);
 
     const onChengeCountry = (event) => {
         setCountry(event.target.value);
@@ -17,10 +32,12 @@ const Form = () => {
     }
 
 
+
+
     return (
         <div className={'form'}>
             <h3>введите ваши данные</h3>
-            <input type="text" placeholder={'Страна'} className={'input'} value={coutry} onChange={onChengeCountry} />
+            <input type="text" placeholder={'Страна'} className={'input'} value={country} onChange={onChengeCountry} />
             <input type="text" placeholder={'Улица'} className={'input'} value={street} onChange={onChengeStreet} />
             <select className={'select'} value={subject} onChange={onChengeSubject}>
                 <option value={'physiacal'}>Физ. лицо</option>
